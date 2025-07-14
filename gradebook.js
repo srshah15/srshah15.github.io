bcdcd6d34761fb2ba9f480c75e88ed5f617d0b4c
srshah15 <raedshah1@gmail.com>
2025-07-14 00:18:43 -0400

    html and js

---
+++ gradebook.js
@@ -0,0 +1,15 @@
+// Stub: fetchGradeData
+function fetchGradeData() {
+    console.log("fetchGradeData() called");
+    // Future code will retrieve data from PostgreSQL
+}
+
+// Stub: populateGradebook
+function populateGradebook(data) {
+    console.log("populateGradebook() called with data:", data);
+    // Future code will use 'data' to populate the gradebook table
+}
+
+// Call the stubs for testing
+fetchGradeData();
+populateGradebook([]);
